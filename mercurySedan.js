8deb5562263e4822731b389e934025f4d5b7d125
Andrew Acin <andrew_acin@hotmail.com>
2024-03-25 13:43:57 -0700

    first commit

--- mercurySedan.js
+++ mercurySedan.js
@@ -9,18 +9,63 @@ console.log(v.make)
 //After you write the derived Car class, you should test it out.
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
-
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+}
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+start() {
+    if (this.fuel > 0) {
+        return this.started = true;
+        console.log("engine started...!!!");
+    } else {
+        return this.started = false;
+        console.log("engine cannot start...");
+    }
+}
+
+loadPassenger(num) {   
+    if (this.passengers < this.maximumPassengers) {
+        this.passengers = num;
+        return this.passengers;
+    }   else {
+        console.log(this.model + "Car" + this.make + " is full")
+    }
+}
+
+scheduleService(mileage) {
+    if (this.mileage > 30000) {
+        return this.mileage = true;
+        console.log("service required");
+    } else {
+        return this.mileage = false;
+        console.log("good to go")
+    }
+}
+
+//Test Cars
+
+let car1 = new Car("Toyota", "Camry", "2023", "Red", "2000")
+
+car1.start()
+car1.loadPassenger(5)
+car1.stop()
+car1.checkService()
+
+console.log(car1.make)
 
 
-
-
-
-
-
-
+const car2 = new Car("Hyundai", "Palisade", "2022", "Black", "5000" )
+console.log(car2.make)
 
 
 
