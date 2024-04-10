10ff6fcc4493e6116165f673db8dbb4ae7310670
Andrew Acin <andrew_acin@hotmail.com>
2024-04-09 21:23:37 -0700

    commiting cars finaly

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,10 +1,8 @@
-const VehicleModule = require("./vehicle").Vehicle
+// const { Vehicle } = require("./vehicleBaseClass.js");
 
-
-
-class Car extends VehicleModule {
+class Car extends Vehicle {
     constructor(make, model, year, color, mileage) {
-        super(make, model, year, color, mileage)
+        super(make, model, year, color, mileage);
         this.maximumPassengers = 5;
         this.passengers = 0;
         this.numberOfWheels = 4;
@@ -12,61 +10,50 @@ class Car extends VehicleModule {
         this.fuel = 10;
         this.scheduleService = false;
     }
+
+    checkService() {
+        if (this.mileage > 30000) {
+            console.log("Service required");
+            this.scheduleService = true;
+        } else {
+            console.log("No service required");
+            this.scheduleService = false;
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            this.started = true;
+            console.log("Engine started...!!!");
+        } else {
+            this.started = false;
+            console.log("Engine cannot start...");
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passengers < this.maximumPassengers) {
+            this.passengers = num;
+            console.log(`${this.model} Car ${this.make} loaded ${num} passengers.`);
+        } else {
+            console.log(`${this.model} Car ${this.make} is full.`);
+        }
+    }
 }
 
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+// Test Cars
 
-checkService((mileage) => {
-    if (this.mileage > 30000) {
-        return this.mileage = true;
-        console.log("service required");
-    } 
-})
+let car1 = new Car("Toyota", "Camry", "2023", "Red", "2000");
+car1.start();
+car1.loadPassenger(5);
+car1.stop(); // You need to define the stop() method in the Car class
+car1.checkService();
 
-start(() => {
-    if (this.fuel > 0) {
-        return this.started = true;
-        console.log("engine started...!!!");
-    } else {
-        return this.started = false;
-        console.log("engine cannot start...");
-    }
-})
+console.log(car1.make);
 
-loadPassenger((num) => {   
-    if (this.passengers < this.maximumPassengers) {
-        this.passengers = num;
-        return this.passengers;
-    }   else {
-        console.log(this.model + "Car" + this.make + " is full")
-    }
-})
+const car2 = new Car("Hyundai", "Palisade", "2022", "Black", "5000");
+car2.start();
+car2.loadPassenger(2);
+car2.stop();
+console.log(car2.make);
 
-
-
-//Test Cars
-
-let car1 = new Car("Toyota", "Camry", "2023", "Red", "2000")
-
-car1.start()
-car1.loadPassenger(5)
-car1.stop()
-car1.checkService()
-
-console.log(car1.make)
-
-
-const car2 = new Car("Hyundai", "Palisade", "2022", "Black", "5000" )
-console.log(car2.make)
-
-
-
-//TO DO: Creating Instances and Testing Them
-
-//You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
-
-
-//Create at least two new instances of the Car class and test them here:
